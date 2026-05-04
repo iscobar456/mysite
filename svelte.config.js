3df4eb6f27fad93d0075eab6d23e4fe4054e8d7c
isaac <isaacspencer17@gmail.com>
2026-05-03 22:14:31 -0600

    switched adapter to node

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,11 @@
 import { mdsvex } from 'mdsvex';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
-	preprocess: [vitePreprocess(), mdsvex()],
-	kit: { adapter: adapter() },
-	extensions: ['.svelte', '.svx']
+    preprocess: [vitePreprocess(), mdsvex()],
+    kit: { adapter: adapter() },
+    extensions: ['.svelte', '.svx']
 };
 
 export default config;
