ee3bb3367b3bb4329be25b7435244ac8c17fb05d
Isaac Spencer <isaacspencer17@gmail.com>
2026-09-15 12:32:12 -0600

    updated landing page and resume

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,7 +7,7 @@
   <h1 class="text-5xl mt-[25vh] mb-6">Isaac Spencer</h1>
   <p class="mb-3">An undergraduate at BYU.</p>
   <p>
-    Check out my <a href="/blog/" class="underline">blog</a> or
+    Check out my <a href="/resume/" class="underline">résumé</a> or
     <a href="mailto:oeapyet0p@mozmail.com" class="underline">send me an email</a>.
   </p>
 </div>
